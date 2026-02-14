4a895f4c14ad2b8a297f53f9af38612b38651adc
Lucas Espindula <lucasespindula23@gmail.com>
2026-02-14 11:07:36 -0300

    Adicionando pasta poo2 com exercícios DLL

---
+++ poo2/AppValidacoesConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BibliotecaValidacoes;
+
+class Program
+{
+    static void Main()
+    {
+        Validador val = new Validador();
+
+        Console.Write("Digite CPF: ");
+        string cpf = Console.ReadLine();
+
+        bool valido = val.ValidadorCPF(cpf);
+
+        Console.WriteLine("CPF válido: " + valido);
+
+        Console.ReadLine();
+    }
+}
